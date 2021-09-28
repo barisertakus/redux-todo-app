57cbda4975ed8965bb9d2de8d70935e2f6735408
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-09-29 02:18:26 +0300

    todoSlice added.

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./TodoItem.css"
+
+function TodoItem({name, done, id}) {
+  return (
+    <div className="todoItem">
+      
+    </div>
+  )
+}
+
+export default TodoItem
