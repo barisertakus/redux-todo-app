6ceb686feb4bef5fc5e81c12ad6cd46f1d3928e9
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-09-29 21:29:04 +0300

    App style updated.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import React from 'react';
-import './App.css';
-import Input from './components/Input';
-import TodoItem from './components/TodoItem';
-
-const todoList = [{
-  item: "todo",
-  done: false,
-  id: Date.now()
-},
-{
-  item: "todo2",
-  done: false,
-  id: Date.now()
-},
-{
-  item: "todo3",
-  done: false,
-  id: Date.now()
-},
-{
-  item: "todo4",
-  done: false,
-  id: Date.now()
-}]
+import React from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Input from "./components/Input";
+import TodoItem from "./components/TodoItem";
+import { selectTodoList } from "./features/todoSlice";
 
 function App() {
+  const todoList = useSelector(selectTodoList);
+
   return (
     <div className="app">
-      <div class="app__container">
-        <div class="app__todo--container">
-        {
-          todoList.map(item=>(
-            <TodoItem name={item.item}
-            done={item.done}
-            id={item.id}
-            />
-          ))
-        
-        }
+      <div className="app__container">
+        <h3>Todo APP</h3>
+        <div className="app__todo-container">
+          {todoList.map((item, i) => (
+            <TodoItem key={i} name={item.item} done={item.done} id={item.id} />
+          ))}
         </div>
+        <Input />
       </div>
-      <Input />
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,23 +2,25 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { saveTodo } from "../features/todoSlice";
 import "./Input.css";
+
 function Input() {
   const [input, setInput] = useState("");
   const dispatch = useDispatch();
 
   const addTodo = () => {
-    console.log("Adding " + input)
-
-    dispatch(saveTodo({
-      item: input,
-      done: false,
-      id: Date.now()
-    }))
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    );
+    setInput("");
   };
 
-  const handleChange = (e) =>{
+  const handleChange = (e) => {
     setInput(e.target.value);
-  }
+  };
 
   return (
     <div className="input">
