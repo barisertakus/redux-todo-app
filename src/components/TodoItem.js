32777bcb18cd227ed2fd815ec3cd15862d2b05b8
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-09-29 21:28:07 +0300

    TodoItem and material ui added.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import "./TodoItem.css"
+import { Checkbox } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setDone } from "../features/todoSlice";
+import "./TodoItem.css";
 
-function TodoItem({name, done, id}) {
+function TodoItem({ name, done, id }) {
+  const dispatch = useDispatch();
+
+  const handleCheck = (e) => {
+    dispatch(setDone(id));
+  };
+
   return (
     <div className="todoItem">
-      
+      <Checkbox checked={done} color="primary" onChange={handleCheck} />
+      <p className={done ? "todoItem--done" : undefined}>{name}</p>
     </div>
-  )
+  );
 }
 
-export default TodoItem
+export default TodoItem;
